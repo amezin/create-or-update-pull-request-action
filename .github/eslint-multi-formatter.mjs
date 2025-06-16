f2da451461fcf9b75aed694adfbf08768838c44b
Aleksandr Mezin <mezin.alexander@gmail.com>
2025-06-16 03:13:12 +0300

    Use shared configs and workflows

--- .github/eslint-multi-formatter.mjs
+++
@@ -1,8 +0,0 @@
-import codeframe from 'eslint-formatter-codeframe';
-import sarif from '@microsoft/eslint-formatter-sarif';
-
-export default function formatter(results, data) {
-    console.log(codeframe(results, data));
-
-    return sarif(results, data);
-}
